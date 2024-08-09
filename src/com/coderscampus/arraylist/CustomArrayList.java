e42f2dcaca2ab689e9069aa121c4e8abc5a07fe1
Jean Arnaud Tanoe <jatan60@gmail.com>
2024-08-09 00:02:37 -0500

    Refactored CustomArrayList.java

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -1,26 +1,23 @@
 package com.coderscampus.arraylist;
 
+import java.util.Arrays;
+
 public class CustomArrayList<T> implements CustomList<T> {
 
 	private Object[] items = new Object[10];
-	private int index = 0; // Count the number of item to add
+	private int size = 0; // Count the number of item to add
 
 	@Override
 	public boolean add(T item) {
-		// If the current index is greater or equal the array length,
-		// then we resize the array
-		if (this.index >= this.getSize()) this.resizeArray();
-		// the item's value at position index is null, then assign the item 
-		if (this.items[index] == null) this.items[index] = item;
-		// Increment the index value for the next item
-		this.index++;
-		// Then return true
+		if (this.size >= this.getSize()) this.resizeArray();
+		if (this.items[size] == null) this.items[size] = item;
+		this.size++;
 		return true;
 	}
 	
 	@Override
 	public int getSize() {
-		return this.items.length;
+		return this.size;
 	}
 
 	@SuppressWarnings("unchecked")
@@ -33,14 +30,8 @@ public class CustomArrayList<T> implements CustomList<T> {
 		return (T) this.items[index];
 	}
 
-	private void resizeArray() {
-		// Initialize and assign a new array of Objects with double 
-		// the size of the current array
-		Object[] newArray = new Object[this.getSize() * 2];		
-		// Copy the current array into the new one
-		System.arraycopy(this.items, 0, newArray, 0, this.getSize());
-		// Assign the new array the current one
-		this.items = newArray;
+	private void resizeArray() {		
+		this.items = Arrays.copyOf(this.items, this.items.length * 2);
 	}
 
 }
